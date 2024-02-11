88cc8f0e3b9daaaf62343b26bc23b085d8a9ff51
LeThanhHung <thanhhung1243it@gmail.com>
2024-02-11 14:33:35 +0700

    fix-layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import HomeTemplate from "./homeTemplate/HomeTemplate";
+import HomePage from "./pages/homePage/HomePage";
 
 function App() {
   return (
@@ -9,6 +11,9 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/register" element={<Register />}></Route>
+        <Route path="" element={<HomeTemplate />}>
+          <Route index element={<HomePage />}></Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   );
