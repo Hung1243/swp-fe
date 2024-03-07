7f13c8e2b53e9bcb5f5544da71d11f23e8f48700
LeThanhHung <thanhhung1243it@gmail.com>
2024-03-07 17:07:57 +0700

    update-callApi

--- src/pages/homePage/MyCourseDetail.jsx
+++ src/pages/homePage/MyCourseDetail.jsx
@@ -2,33 +2,13 @@ import { Button, Col, Collapse, Row, Space } from "antd";
 import React, { useEffect, useState } from "react";
 import api from "../../config/axios";
 import { useParams } from "react-router";
-const text = `
-  
-`;
-const items = [
-  {
-    key: "1",
-    label: "This is panel header 1",
-    children: <p>{text}</p>,
-  },
-  {
-    key: "2",
-    label: "This is panel header 2",
-    children: <p>{text}</p>,
-  },
-  {
-    key: "3",
-    label: "This is panel header 3",
-    children: <p>{text}</p>,
-  },
-];
 
 const MyCourseDetail = () => {
   const params = useParams();
   const onChange = (key) => {
     console.log(key);
   };
-  const [myCourseDetail, setMyCourseDetail] = useState([]);
+  const [myCourseDetail, setMyCourseDetail] = useState(null);
   const getCourseDetail = async () => {
     const res = await api.get(`/courseDetail/${params.id}`);
     setMyCourseDetail(res.data);
@@ -38,14 +18,40 @@ const MyCourseDetail = () => {
     getCourseDetail();
   }, []);
 
+  const [items, setItems] = useState([]);
+  // const [lessons, setLessons] = useState([]);
+
+  useEffect(() => {
+    if (myCourseDetail) {
+      setItems(
+        myCourseDetail.chapters.map((items) => {
+          return {
+            key: "1",
+            label: items.name,
+            children: myCourseDetail?.lessons.map((items) => {
+              return (
+                <ul>
+                  <li className="list-unstyled ">{items.name}</li>
+                </ul>
+              );
+            }),
+          };
+        })
+      );
+    }
+  }, [myCourseDetail]);
+
   return (
     <>
       <section id="my-course-detail">
         <div className="my-course-content">
           <Row gutter={24}>
             <Col span={18}>
-              <video width="1100" height="100%" controls>
-                <source src={myCourseDetail.videoLink} type="video/mp4" />
+              <video width="1170" height="100%" controls>
+                <source
+                  src={myCourseDetail?.lessons[0].videoLink}
+                  type="video/mp4"
+                />
               </video>
             </Col>
             <Col span={6}>
