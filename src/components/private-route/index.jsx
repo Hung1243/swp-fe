35481318c6112a4032aaba75523f569962c5b4a0
LeThanhHung <thanhhung1243it@gmail.com>
2024-03-21 22:20:15 +0700

    fix/private-route

--- src/components/private-route/index.jsx
+++ src/components/private-route/index.jsx
@@ -10,7 +10,7 @@ const PrivateRoute = ({ role }) => {
     <Result
       status="403"
       title="403"
-      subTitle="Sorry, you are not authorized to access this page."
+      subTitle="Sorry, you are not authorized to access this page !!!"
       extra={<Button type="primary">Back Home</Button>}
     />
   );
